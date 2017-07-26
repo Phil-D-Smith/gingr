c49e3661af40501f22812a67274274a85445dada
Phil <phil.1496@googlemail.com>
2017-07-26 22:08:20 +0100

    adding login,db,signup, not currently working

---
+++ Server/signup.php
@@ -0,0 +1,27 @@
+<?php
+	header("access-control-allow-origin: *");
+
+	include("config.php");
+
+	if(isset($_POST['signup'])) {
+		$firstname=mysql_real_escape_string(htmlspecialchars(trim($_POST['firstname'])));
+		$lastname=mysql_real_escape_string(htmlspecialchars(trim($_POST['lastname'])));
+		$email=mysql_real_escape_string(htmlspecialchars(trim($_POST['email'])));
+		$password=mysql_real_escape_string(htmlspecialchars(trim($_POST['password'])));
+
+		$login=mysql_num_rows(mysql_query("select * from `phonegap_login` where `email`='$email'"));
+		if($login!=0) {
+			echo "exist";
+		}else {
+			$date=date("y-m-d h:m:s");
+			$q=mysql_query("insert into `user_table` (`reg_date`,`firstname`,'lastname',`email`,`password`) values ('$date','$fullname','$email','$password')");
+			if($q) {
+				echo "success";
+			}else {
+				echo "failed";
+			}
+		}
+		echo mysql_error();
+	}
+
+?>
