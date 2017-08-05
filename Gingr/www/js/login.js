ff131b0f0293e88ca8f09e574099de21e63195e1
Phil <phil.1496@googlemail.com>
2017-08-05 13:23:42 +0100

    virtual host added for server, forget phonegap it is crap

--- Gingr/www/js/login.js
+++ Gingr/www/js/login.js
@@ -1,61 +1,88 @@
 //initialize
-var myApp = new Framework7();
+//var myApp = new Framework7();
 
 //custom DOM
-var $$ = Dom7;
+//var $$ = Dom7;
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
 
-	console.log("clicked");
+	//wait for login page to be fully loaded
+	myApp.onPageInit('signup', function (page) {
+
+		console.log("login ready");
+
+		//event listener for signup button
+		var signupButton = document.getElementById("signup");
+
+		if(signupButton) {
+			signupButton.addEventListener("click", signup, false);
+		}
 	
-	//on click of signup button
-	$("#signup").click(function() {
+		//on click of signup button
+		function signup() {
 
-		console.log("clicked");
+			console.log("clicked");
 
+			//get values of form into variables
+			var firstname = $("#firstname").val();
+			var lastname = $("#lastname").val();
+			var email = $("#email1").val();
+			var password = $("#password1").val();
 
-		//get values of form into variables
-		var firstname = $("#firstname").val();
-		var lastname = $("#lastname").val();
-		var email = $("#email").val();
-		var password = $("#password").val();
+			//create data array
+			var dataString = {firstname: firstname, lastname: lastname, email: email, password: password};
+			/*var dataString =	"&firstname="+firstname+
+								"&lastname="+lastname+
+								"&email="+email+
+								"&password="+password;*/
 
-		//string and serialize
-		var dataString =	"&firstname="+firstname+
-							"&lastname="+lastname+
-							"&email="+email+
-							"&password="+password;
+			console.log(dataString);
 
+			//if form isnt empty, post ajax request to server
+			if($.trim(firstname).length > 0 & $.trim(email).length > 0 & $.trim(password).length > 0) {
 
-		//if form isnt empty, post ajax request to server
-		if($.trim(firstname).length>0 & $.trim(email).length>0 & $.trim(password).length>0) {
-			$.ajax({
-				type: "POST",
-				url: "http://localhost:80/gingr_server/signup.php",
-				async: false,
-				data: dataString,
-				crossDomain: true,
-				cache: false,
+				console.log("input checked");
 
-				beforeSend: function() {
-					$("#signup").val('Connecting...');
-					console.log("connecting to server");
-				},
+				$.ajax({
+					type: "POST",
+					url: "localhost:80/gingr_server/signup.php",
+					data: dataString,
+					crossDomain: true,
+					cache: false,
 
-				//display success/fail message
-				success: function(data) {
-					if(data == "success") {
-						alert("Sign up successful");
-						console.log("connection successful - signed up");
-					}else if(data = "exist") {
-						alert("Already have an account");
-						console.log("connection successful - user already exists");
-					}else if(data = "failed") {
-						alert("Something went wrong");
+					beforeSend: function() {
+						$("#signup").val('Connecting...');
+						console.log("connecting to server");
+					},
+
+					//display success/fail message - put something in data on server
+					success: function(data) {
+						if(data == "success") {
+							console.log("connection successful - signed up");
+						} else if(data == "exist") {
+							console.log("connection successful - user already exists");
+						} else if(data == "failed") {
+							console.log("something went wrong");
+						}
+					},
+
+					error: function(xhr, ajaxOptions, errorThrown) {
+						console.log("error msg: ");
+						console.log(xhr);
+						console.log(errorThrown);
+					},
+
+					complete: function(data) {
+						if(data.readyState == "0") {
+							console.log("unsent");
+						} else if(data.readyState == "4") {
+							console.log("done");
+						}	
 					}
-				}
-			});
-		} return false;
+
+				});
+			} return false;
+		}
 	});
 });
--- gingr_server/signup.php
+++ gingr_server/signup.php
@@ -1,14 +1,18 @@
+<html>
+
 <?php
 	header("access-control-allow-origin: *");
 	header("access-control-allow-methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+	header("access-control-allow-headers: X-Requested-With, Content-Type");
 
 	require_once "config.php";
+	echo "on server";
 
 	if(isset($_POST['signup'])) {
-		$firstname = mysql_real_escape_string(htmlspecialchars(trim($_POST["firstname"])));
-		$lastname = mysql_real_escape_string(htmlspecialchars(trim($_POST["lastname"])));
-		$email = mysql_real_escape_string(htmlspecialchars(trim($_POST["email"])));
-		$password = mysql_real_escape_string(htmlspecialchars(trim($_POST["password"])));
+		$firstname = mysql_real_escape_string(htmlspecialchars($_POST["firstname"]));
+		$lastname = mysql_real_escape_string(htmlspecialchars($_POST["lastname"]));
+		$email = mysql_real_escape_string(htmlspecialchars($_POST["email"]));
+		$password = mysql_real_escape_string(htmlspecialchars($_POST["password"]));
 
 		$login = mysql_num_rows(mysql_query("SELECT * FROM user_table WHERE email = $email"));
 		if($login != 0) {
@@ -28,3 +32,5 @@
 	}
 
 ?>
+
+</html>
